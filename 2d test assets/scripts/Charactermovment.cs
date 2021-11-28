76e692f56f87d6a0f488e2c7b0c13771204bb9dd
stail/claude <dmwattem@gmail.com>
2021-11-28 17:53:53 -0500

    push

--- 2d test assets/scripts/Charactermovment.cs
+++ 2d test assets/scripts/Charactermovment.cs
@@ -15,12 +15,12 @@ public class Charactermovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float y = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         Vector2 pos = transform.position;
 
-        pos.x += y * speed * Time.deltaTime;
+        pos.x += h * speed * Time.deltaTime;
         pos.y += v * speed * Time.deltaTime;
 
         transform.position = pos;
